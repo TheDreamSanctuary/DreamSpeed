d612d4c7d34b55a3b3bb9d4a61c9034014acc4d3
Joe Kent <thedeadlybutter@gmail.com>
2014-03-30 18:37:47 -0400

    Initial project creation
    Base plugin class, maven, config, plugin, etc

---
+++ src/main/java/com/thedreamsanctuary/dreamspeed/DreamSpeed.java
@@ -0,0 +1,18 @@
+package com.thedreamsanctuary.dreamspeed;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Butterss on 3/30/14.
+ */
+public class DreamSpeed extends JavaPlugin {
+
+    public void onEnable(){
+
+    }
+
+    public void onDisable(){
+
+    }
+
+}
